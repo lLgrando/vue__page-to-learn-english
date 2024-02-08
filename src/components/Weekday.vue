abfe9830a8f90f0ae1e2139f4c6d089711af9530
Leonardo <llggrando@gmail.com>
2024-02-07 22:58:56 -0300

    Adicionando a lógica e estilização da página Weekday.vue.

--- src/components/Weekday.vue
+++ src/components/Weekday.vue
@@ -2,52 +2,50 @@
 import { ref } from 'vue';
 import Title from './global_component/Title.vue';
 
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const questions = [
+    { question: "What day of week is today?", numberTime: 0, userAnswer: null },
+    { question: "What day of week will it be tomorrow?", numberTime: 1, userAnswer: null },
+    { question: "What day of the week will it be in two days?", numberTime: 2, userAnswer: null },
+    { question: "What day of the week will it be in three days?", numberTime: 3, userAnswer: null },
+    { question: "What day of the week will it be in four days?", numberTime: 1, userAnswer: null },
+    { question: "What day of the week will it be in five days?", numberTime: 1, userAnswer: null },
+    { question: "What weekday was yesterday?", numberTime: -1, userAnswer: null },
+]
 
 let answer = ref([]);
 
-function getWeekDayToday() {
+function getWeekDayFromNumber(userAnswer, itemNumber) {
     const date = new Date();
-    let day = weekday[date.getDay()];
-    return day;
+    let expectedDay = weekday[(date.getDay() + questions[itemNumber].numberTime + 7) % 7];
+    userAnswer === expectedDay ? questions[itemNumber].userAnswer = true : questions[itemNumber].userAnswer = false;
+    answer.value = [];
 }
-
-function getWeekDayFromNumber(providedDay, index) {
-    const date = new Date();
-    let expectedDay = weekday[(date.getDay() + index + 7) % 7];
-    
-    if (providedDay === expectedDay) {
-        console.log('Correct!' + " " + providedDay);
-    } else {
-        console.log('Incorrect!'  + " " + providedDay);
-    }
-}
-
 </script>
 
 <template>
     <Title title="What is the day?"></Title>
 
-    <h1>{{ getWeekDayToday() }}</h1>
+    <div class="w-full flex flex-col justify-center items-center">
+        <ul v-for="item, index in questions"
+            class="flex flex-col justify-center items-center border-b border-slate-500 w-full p-6">
+            <li class="sm:text-2xl text-xl w-46">{{ item.question }}</li>
+
+            <li v-if="item.userAnswer === null"></li>
+            <li v-else-if="item.userAnswer === true">
+                <span class="material-symbols-outlined" style="color: green; font-size:38px;">check</span>
+            </li>
+            <li v-else="item.userAnswer === false">
+                <span class="material-symbols-outlined" style="color: red; font-size:38px;">close</span>
+            </li>
 
 
-    <div class="w-full flex justify-center items-center">
-        <span class="text-2xl">Whats weekday is today?</span>
-        <input type="text" v-model="answer[0]" @keyup.enter="getWeekDayFromNumber(answer[0], 0)"
-            class="border border-slate-300 rounded">
+            <input type="text" v-model="answer[index]" :name="index"
+                @keyup.enter="getWeekDayFromNumber(answer[index], index)"
+                class="border border-slate-300 rounded mt-2 w-46 p-2 text-center text-xl">
+        </ul>
     </div>
-
-    <div class="w-full flex justify-center items-center">
-        <span class="text-2xl">Whats weekday was yesterday?</span>
-        <input type="text" v-model="answer[1]" @keyup.enter="getWeekDayFromNumber(answer[1], -1)"
-            class="border border-slate-300 rounded">
-    </div>
-
-
-
-
-
 </template>
 
-<style>
-</style>
+<style></style>
